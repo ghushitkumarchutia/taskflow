082561ba518cd1b0ed6e2bfdb9233a20c187b5f9
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-05-12 00:30:57 +0530

    fix(db): configure @prisma/adapter-pg for direct database connection to resolve v7 initialization error

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-export const prisma = new PrismaClient();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
